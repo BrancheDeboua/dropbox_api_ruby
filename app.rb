a766abce7151b401050fff4b0e4f431b497d3186
Samuel Laforme <laforme.s@hotmail.com>
2022-03-21 16:42:44 -0400

    being able to send file to dropbox

--- app.rb
+++ app.rb
@@ -1,22 +1,11 @@
 require 'excon'
 require 'json'
+require 'dropbox_api'
 
-
-
-
-def send_file
+def send_file(fichier)
     file = File.open('chicken.jpeg')
-    r = Excon.post("https://content.dropboxapi.com/2/files/upload_session/start",
-        :headers => { 'Content-Type' => 'application/octet-stream', 'Authorization' => "Bearer #{ENV['DBKEY']}", 'Dropbox-API-Arg' => '{"close": true}' },
-        :body => file
-    )
-
-    p r.body
-    result = Excon.post("https://content.dropboxapi.com/2/files/upload_session/finish",
-        :headers => { 'Content-Type' => 'application/octet-stream', 'Authorization' => "Bearer #{ENV['DBKEY']}", 'Dropbox-API-Arg' => "{\"cursor\": \"{ \"session_id\": \"#{r.body['session_id']}\", \"offset\": 0 }\"}" },
-        :body => file
-    )
-    p result.body
+    client = DropboxApi::Client.new
+    client.upload_by_chunks "/#{fichier}/patate", file
 end
 
 folder = "henry"
@@ -31,11 +20,11 @@ r = Excon.post('https://api.dropboxapi.com/2/files/create_folder_v2',
 result  = JSON.parse(r.body)
 if result['metadata']
     # Here the folder has been created by the request before, need to create add the file inside the folder
-    send_file
+    send_file folder
 else
     if result['error_summary'] == 'path/conflict/folder/' || result['error_summary'] == 'path/conflict/folder/...' || result['error_summary'] == 'path/conflict/folder/.' || result['error_summary'] == 'path/conflict/folder/..'
         # Here the folder already exists and we can add the file in it already
-        send_file
+        send_file folder
     else
         # Error wtf Jim
         p result['error_summary']
